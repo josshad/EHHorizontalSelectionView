4aeb9b4aad7780bcd7da830b7a0332ad119c1460
Josshad <Josshad@gmail.com>
2016-08-30 13:05:06 +0300

    Initial Commit

---
+++ EHHorizontalSelectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EHHorizontalSelectionView
+//
+//  Created by Danila Gusev on 30/08/2016.
+//  Copyright © 2016 josshad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EHHorizontalSelectionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EHHorizontalSelectionView
+//
+//  Created by Danila Gusev on 30/08/2016.
+//  Copyright © 2016 josshad. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
